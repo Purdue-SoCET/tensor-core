f62a2054e37d21278cf4d1052ecc2fe47cc76922
AkshathRaghav <akshathraghav.r@gmail.com>
2025-03-02 19:09:46 -0500

    Initial push to Saandiya's im2col
    Signed-off-by: AkshathRaghav <akshathraghav.r@gmail.com>

---
+++ src/modules/sysarr_LUF_CACHE.sv
@@ -0,0 +1,17 @@
+`include "cache_types_pkg.svh";
+
+module lockup_free_cache (
+    input logic CLK, nRST,
+    input logic mem_in,
+    input logic [3:0] mem_in_uuid,
+    input logic [31:0] mem_in_addr,
+    input logic mem_in_rw_mode, // 0 = read, 1 = write
+    input logic [31:0] mem_in_store_value,
+    output logic stall,
+    output logic hit,
+    output logic [NUM_BANKS-1:0] block_status,
+    output logic [NUM_BANKS-1:0][3:0] uuid_block
+);
+
+
+endmodule
