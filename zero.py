9d997af1898118821fa144e921d852b25eb51f2e
rrbathina <rrbathina@gmail.com>
2025-07-23 15:20:30 -0400

    working on mem

--- zero.py
+++ zero.py
@@ -1,19 +1,16 @@
+# zeroing non instr/data in meminit for bram mem
 
 input_file = "/home/asicfab/a/rrbathin/socet/amp/tensor-core/src/meminit.mem"
-memory_size = 65536  # 64K addresses
+memory_size = 65536 
 
-# Read existing lines
 with open(input_file, "r") as f:
     lines = [line.strip() for line in f.readlines()]
 
-# Pad with 00000000 if file is too short
 while len(lines) < memory_size:
     lines.append("00000000")
 
-# Truncate if file is too long (optional safety step)
 lines = lines[:memory_size]
 
-# Overwrite original file
 with open(input_file, "w") as f:
     for line in lines:
         f.write(line + "\n")
