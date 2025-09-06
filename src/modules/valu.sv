4c26d74a9553acdc087fcbd4e12b0574381f494f
Joseph Alan Ghanem <jghanem@asicfab.ecn.purdue.edu>
2025-09-06 18:50:11 -0400

    vector top level + valu top level w/ loose skelelton of the special cases

--- src/modules/valu.sv
+++ src/modules/valu.sv
@@ -1,20 +1,52 @@
 `include "vector_if.vh"
+`include "vector_types.vh"
  
  module vector_alu (
-   vector_if.valu vif
+  input logic CLK, nRST,
+  vector_if.valu vif
  );
- 
-    always_comb begin
-        // Special Cases
+  import vector_pkg::*;
 
-        // Normalize inputs
-        
+    always_comb begin : FP16_SPECIALCASES
+      // Zero
+      if (vif.vdat1.exp == 5'd0 && vif.vdat1.frac == 10'd0) begin
 
-        casez (vif.vop)
-            VALU_ADD: 
+      end
 
-            default: v_if.result = '0;
-        endcase
+      // Subnormal
+      if (vif.vdat1.exp == 5'd0 && vif.vdat1.frac != 10'd0) begin
+
+      end
+
+      // Inf
+      if (vif.vdat1.exp == 5'd255 && vif.vdat1.frac == 10'd0) begin
+
+      end
+
+      // NaN
+      if (vif.vdat1.exp == 5'd255 && vif.vdat1.frac != 10'd0) begin
+
+      end
     end
- 
+    
+    genvar lane;
+    generate
+      for (lane = 0; lane < NUM_ELEMENTS; lane++) begin : GEN_LANES
+        always_comb begin : VALU_LANE
+          if (!vif.vm || vif.vmask[lane]) begin
+            casez (vif.vop)
+                VALU_ADD: begin
+                  vif.result[lane] = vfpadd(vif.vdat1[lane], vif.vdat2[lane]);
+                end 
+                VALU_SUB: begin
+                  vif.vdat2[lane].sign = ~vif.vdat2[lane].sign;
+                  vif.result[lane] = vfpadd(vif.vdat1[lane], vif.vdat2[lane]);
+                end
+
+                default: vif.result[lane] = '0;
+            endcase
+          end
+        end
+      end
+    endgenerate
  endmodule
