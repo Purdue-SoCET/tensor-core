7451d91ce45c3ffc9cbbab0ec0b55d268b56ebc0
Joseph <sowersj@purdue.edu>
2025-10-09 16:54:56 -0400

    lfc_ram_transaction corrections

--- uvm/lfc_ram_transaction.svh
+++ uvm/lfc_ram_transaction.svh
@@ -5,20 +5,23 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class lfc_ram_transaction #(parameter NUM_BANKS = 4) extends uvm_sequence_item;
-    rand logic [NUM-1:0][31:0] ram_mem_data;
+    logic n_rst;
+    rand logic [NUM_BANKS-1:0][31:0] ram_mem_data;
+    logic [NUM_BANKS-1:0] ram_mem_complete;
 
     logic [NUM_BANKS-1:0] ram_mem_REN;
     logic [NUM_BANKS-1:0] ram_mem_WEN;
-    logic [NUM-1:0][31:0] ram_mem_addr;
-    logic [NUM-1:0][31:0] ram_mem_store;
+    logic [NUM_BANKS-1:0][31:0] ram_mem_addr;
+    logic [NUM_BANKS-1:0][31:0] ram_mem_store;
 
 
     `uvm_object_utils_begin(lfc_ram_transaction)
-        `uvm_field(ram_mem_data, UVM_DEFAULT);
-        `uvm_field(ram_mem_REN, UVM_DEFAULT);
-        `uvm_field(ram_mem_WEN, UVM_DEFAULT);
-        `uvm_field(ram_mem_addr, UVM_DEFAULT);
-        `uvm_field(ram_mem_store, UVM_DEFAULT);
+        `uvm_field_int(ram_mem_data, UVM_DEFAULT);
+        `uvm_field_int(ram_mem_complete, UVM_DEFAULT);
+        `uvm_field_int(ram_mem_REN, UVM_DEFAULT);
+        `uvm_field_int(ram_mem_WEN, UVM_DEFAULT);
+        `uvm_field_int(ram_mem_addr, UVM_DEFAULT);
+        `uvm_field_int(ram_mem_store, UVM_DEFAULT);
     `uvm_object_utils_end
 
     // no current randomization constraints
