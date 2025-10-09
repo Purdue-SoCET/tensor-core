128be07c83dc23d64c2b9ea8b03a901cba909c9e
Joseph <sowersj@purdue.edu>
2025-10-09 16:47:44 -0400

    lfc_ram_transaction first attempt

--- uvm/lfc_ram_transaction.svh
+++ uvm/lfc_ram_transaction.svh
@@ -5,10 +5,27 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class lfc_ram_transaction #(parameter NUM_BANKS = 4) extends uvm_sequence_item;
-    rand bit [NUM-1:0][31:0] ram_mem_data;
+    rand logic [NUM-1:0][31:0] ram_mem_data;
+
+    logic [NUM_BANKS-1:0] ram_mem_REN;
+    logic [NUM_BANKS-1:0] ram_mem_WEN;
+    logic [NUM-1:0][31:0] ram_mem_addr;
+    logic [NUM-1:0][31:0] ram_mem_store;
+
 
     `uvm_object_utils_begin(lfc_ram_transaction)
-        `uvm_field
+        `uvm_field(ram_mem_data, UVM_DEFAULT);
+        `uvm_field(ram_mem_REN, UVM_DEFAULT);
+        `uvm_field(ram_mem_WEN, UVM_DEFAULT);
+        `uvm_field(ram_mem_addr, UVM_DEFAULT);
+        `uvm_field(ram_mem_store, UVM_DEFAULT);
+    `uvm_object_utils_end
+
+    // no current randomization constraints
+
+    function new(string name = "lfc_ram_transaction");
+        super.new(name);
+    endfunction
 
 endclass
 
