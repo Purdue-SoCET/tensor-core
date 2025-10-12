999303b6b5a61d3a2b066c6278923c26236c4713
Nicha Muninnimit <nmuninni@purdue.edu>
2025-10-12 16:47:55 -0400

    fixed IDX parameter in top scratchpad and typos in scratchpad_if

--- rtl/modules/memory/scratchpad/scratchpad.sv
+++ rtl/modules/memory/scratchpad/scratchpad.sv
@@ -10,9 +10,9 @@ module scratchpad (
     genvar ti;
     generate
         for (ti = 0; ti < NUM_SCPADS; ti++) begin : g_scpad
-            frontend #(.IDX(IDX)) u_frontend (.clk(clk), .n_rst(n_rst), .fcif(sif));
-            backend #(.IDX(IDX)) u_backend (.clk(clk), .n_rst(n_rst), .bif(sif));
-            body #(.IDX(IDX)) u_body (.clk(clk), .n_rst(n_rst), .bif(sif));
+            frontend #(.IDX(ti)) u_frontend (.clk(clk), .n_rst(n_rst), .fcif(sif));
+            backend #(.IDX(ti)) u_backend (.clk(clk), .n_rst(n_rst), .bif(sif));
+            body #(.IDX(ti)) u_body (.clk(clk), .n_rst(n_rst), .bif(sif));
         end
     endgenerate
 
