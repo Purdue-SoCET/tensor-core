b76704bbf4cb2669212c641a6f0b867cfa796c93
socet149 <socet149@asicfab.ecn.purdue.edu>
2025-10-17 14:15:36 -0400

    frontend done?, still need frontend_vc

--- rtl/modules/memory/scratchpad/frontend.sv
+++ rtl/modules/memory/scratchpad/frontend.sv
@@ -15,13 +15,13 @@ module frontend #(parameter logic [SCPAD_ID_WIDTH-1:0] IDX = '0) (scpad_if.front
     scpad_if.frontend_vc vcif();
 
     // instantiate internal frontend_vc
-    frontend_vc #(.IDX(IDX)) u_frontend_vc (.vcif(vcif));
+    frontend_vc #(.IDX(IDX)) u_frontend_vc (.clk(fvif.clk), .nRST(fvif.n_rst), .vcif(vcif));
 
     // latch incoming request from Vector Core into the FE->Body path
     // only latch when the incoming request is valid and pipeline enabled
     latch #(.T(req_t)) u_latch_req (
-        .clk (clk),
-        .n_rst(n_rst),
+        .clk (fvif.clk),
+        .n_rst(fvif.n_rst),
         .en  (pipe_EN && fvif.vec_req[IDX].valid),
         .in  (fvif.vec_req[IDX]),
         .out (fsif.fe_req[IDX])
@@ -29,8 +29,8 @@ module frontend #(parameter logic [SCPAD_ID_WIDTH-1:0] IDX = '0) (scpad_if.front
 
     // latch response from Body back to Vector Core
     latch #(.T(res_t)) u_latch_res (
-        .clk (clk),
-        .n_rst(n_rst),
+        .clk (fvif.clk),
+        .n_rst(fvif.n_rst),
         .en  (pipe_EN && fsif.fe_res[IDX].valid),
         .in  (fsif.fe_res[IDX]),
         .out (fvif.vec_res[IDX])
