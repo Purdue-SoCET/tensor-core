5387d0f0d4474bc4f6f8a369d7256ca5cfde7738
herna628 <142940226+herna628@users.noreply.github.com>
2025-10-26 15:45:27 -0400

    backend tb start

---
+++ tb/unit/memory/scratchpad/backend_tb.sv
@@ -0,0 +1,86 @@
+`timescale 1ps/1ps
+
+`include "scpad_if.sv"
+import scpad_pkg::*;
+
+// // Scheduler <=> Backend
+//     modport backend_sched (
+//         input clk, n_rst, sched_req,
+//         output sched_res
+//     );
+
+//     // Backend <=> Body
+//     modport backend_body (
+//         input clk, n_rst, 
+//         input  be_stall, be_res, 
+//         output be_req
+//     );
+
+//     // Backend <=> DRAM
+//     modport backend_dram (
+//         input clk, n_rst, 
+//         output be_dram_req, be_dram_stall,
+//         input dram_be_res
+//     );
+
+module backend_tb;
+
+    localparam CLK_PERIOD = 10; 
+    
+    logic clk, n_rst;
+
+    always #(CLK_PERIOD/2) clk = ~clk;
+
+    scpad_if bif(clk, n_rst);
+
+    // module backend #(parameter logic [SCPAD_ID_WIDTH-1:0] IDX = '0) (
+    // scpad_if.backend_sched bsbif, 
+    // scpad_if.backend_body bscif, 
+    // scpad_if.backend_dram bdrif
+    // );
+    
+    backend #(.IDX(0)) DUT (bif);
+
+    initial begin
+        n_rst = 0;
+        repeat (5) @(posedge clk);
+        n_rst = 1;
+    end
+
+    string fname, wavepath; 
+    getenv("WAVEPATH", wavepath);
+    $sformat(fname, "%s/head_tb.vcd", wavepath); 
+
+    // initial begin 
+    //     $dumpfile(fname);
+    //     $dumpvars(0);
+    // end 
+
+    test PROG (.bif(bif)); 
+
+    initial begin
+        #(10_000 * CLK_PERIOD) $fatal(1, "[TB] Timeout");
+    end
+
+endmodule
+
+program test (scpad_if.backend_tb bif);
+
+    task reset(); 
+        bif.n_rst = 0;
+        repeat (2) @(posedge bif.clk);
+        bif.n_rst = 1;
+        repeat (2) @(posedge bif.clk);
+    endtask
+
+
+    initial begin
+
+        bif = 0;
+
+        reset();
+        #(CLK_PERIOD * 2);
+
+    end 
+
+endprogram
